7d8a226e941e00647f79241236e53beefb4ca624
ArturH16 <holandaartur163@gmail.com>
2025-09-14 17:07:28 -0300

    Criação do ex008, ex009 e ex010

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+console.log('Olá,Mundo!')
+velocidade = 80
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+if (velocidade >= 80) { //CONDIÇÃO SIMPLES
+    console.log(`EITA MENINO E DEIXOU O ARROZ NO FOGO FOI??`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+let pais = 'BRASIL'.toLowerCase()
+console.log(`Vivendo em ${pais}`)
+if (pais == 'brasil') { //Condição composta
+    console.log('BRASILEIRO')   
+} else {
+    console.log('ESTRANGEIRO')
+}
