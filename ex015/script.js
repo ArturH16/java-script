fd2a34602ca563695e3581cd6f78e219a8579cd2
ArturH16 <holandaartur163@gmail.com>
2025-09-21 15:22:17 -0300

    Finalização do ex015

--- ex015/script.js
+++ ex015/script.js
@@ -5,18 +5,39 @@ function mostrar() {
 let resultado = document.getElementById('res')
 let txtano = document.getElementById('ano')
 let ano = Number(txtano.value)
-let sexoM = document.getElementById('sexom').value
-let sexof = document.getElementById('sexof').value
 let ano_atual = new Date().getFullYear()
 
-if(ano > ano_atual) {
+if(ano > ano_atual || txtano.value.length == 0) {
     resultado.innerHTML = 'Esse ano não é aceito pelo sistema!'
 }  else {
-    if (ano_atual - 18 == ano) {
-        resultado.innerHTML = `Detectamos uma mulher de ${ano_atual - ano} anos <br>`
-        resultado.innerHTML += `<img src='imagens/adulta.png'>`
+    let sexo = document.getElementsByName('sex')
+    let idade = ano_atual - ano
+    let genero = ''
+    if (sexo[0].checked) {
+        genero = 'Homem'
+    } else {
+        genero = 'Mulher'
+    } if (genero == 'Homem') {
+        if (idade <= 65 && idade >= 18) {
+            resultado.innerHTML = `<p>Detectamos um homem de ${idade} anos</p> <p> <img src = 'imagens/adulto-p.png'> </p>`
+        } else if (idade > 65) {
+            resultado.innerHTML = `<p>Detectamos um homem de ${idade} anos</p> <p> <img src = 'imagens/idoso-p.png'> </p>`
+        } else {
+            resultado.innerHTML = `<p>Detectamos um homem de ${idade} anos</p> <p> <img src = 'imagens/criança-m-p.png'> </p>`
+        } 
+    
+    } else {
+        if (idade <= 65 && idade >= 18) {
+            resultado.innerHTML = `<p>Detectamos uma mulher de ${idade} anos</p> <p> <img src = 'imagens/adulta-p.png'> </p>`
+        } else if (idade > 65) {
+            resultado.innerHTML = `<p>Detectamos uma mulher de ${idade} anos</p> <p> <img src = 'imagens/idosa-p.png'> </p>`
+        } else {
+            resultado.innerHTML = `<p>Detectamos uma mulher de ${idade} anos</p> <p> <img src = 'imagens/crianca-f-p.png'> </p>`
+
     }
-}
+  
+} 
+    }
 }
 
 
