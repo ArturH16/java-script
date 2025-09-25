8a6155ce870596535a0ac649f8f3933e06c0714e
ArturH16 <holandaartur163@gmail.com>
2025-09-25 11:35:35 -0300

    Criação dos ambientes de teste

---
+++ aula013/ambiente.js
@@ -0,0 +1,13 @@
+let pizza = 8
+while (pizza != 0) {
+    console.log('Quantidade de pedaços',pizza) 
+    pizza -=1 
+}
+//Aqui vai ser a estrutura de repetição Do While
+
+let pizza2_comidos = 0
+do {
+    console.log('Quantidade de pedaços comidos ' + pizza2_comidos)
+    pizza2_comidos ++
+
+} while (pizza2_comidos <= 10)
---
+++ aula014/ambiente.js
@@ -0,0 +1,3 @@
+for (let pizza_comidas = 0; pizza_comidas <= 10; pizza_comidas ++) {
+    console.log('Pizzas comidas: ' + pizza_comidas)
+}
