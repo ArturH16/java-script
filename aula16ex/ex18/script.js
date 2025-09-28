957c7784b15f34d3b970618616f9414da43df5b7
ArturH16 <holandaartur163@gmail.com>
2025-09-27 21:27:33 -0300

    Adição da solução do Guanabara

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -4,6 +4,8 @@ let botao_adicionar = document.getElementById('adicionar_botao')
 botao_adicionar.addEventListener('click',adicionar)
 let elem = 0
 function adicionar() {
+
+
     res.innerHTML = ''
     let tab = document.getElementById('tab_select')
     let numerotxt = document.getElementById('numero')
@@ -16,9 +18,14 @@ function adicionar() {
         opcao.innerHTML += `Valor ${num} adicionado!`
         tab.appendChild(opcao)
         elem += 1
-        
-
+        numerotxt.value = ''
+        numerotxt.focus()
     }
+        
+    
+
+
+    
 
 let botao_finalizar = document.getElementById('finalizar_botao')
 botao_finalizar.addEventListener('click',finalizar)
@@ -54,7 +61,56 @@ function finalizar() {
 
 
 /* SOLUÇÃO GUANABARA
+let num = document.querySelector(input#num)
+let lista = document.querySelector(div#res)
+let valores = []
 
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+    return true } else {
+        return false}
+}
 
+function inLista(n,l) {
+    if l.indexOf(Number(n)) != -1 {
+    return true } else {
+        return false } 
+        }
+
+function adicionar() {
+if (isNumero(num.value) && !inLista(num.value,valores)) {
+    valores.push(Number(num.value))
+    let item = document.createelement('option')
+    item.text = `Valor ${num.value} adicionado`
+    item.appendChild(item) 
+    res.innerHTML = ''} else {
+      'window.alert('Valor inválido ou já encontrado na lista)  }
+      num.value = ''
+      num.focus()
+}
+
+function finalizar() {
+if (valores.length == 0) {
+window.alert('Adicione valores antes de finalizar!)
+} else {
+    let tot = valores.length
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    let media = 0
+    for (let pos in valores) {
+    soma += valores[pos]
+    if (valores[pos] > maior) {
+    maior = valores[pos]} if (valores[pos] < menor) {
+    menor = valores[pos]}
+    }
+    }
+    media = soma / tot
+    res.innerHTML = ''
+    res.innerHTML += `<pAo todo, temos %{tot} valores cadastrados</p>`
+    res.innerHTML += `<pO maior valor digitado foi %{maior} </p>`
+    res.innerHTML += `<pO menor valor digitado foi %{menor}</p>`
+    res.innerHTML += `<pA média dos valores  digitados é %{media}</p>`
+}
 
 */
